e5af0d5d2dc5e55c28b5dec17309faef6ef67032
bugembe john paul <bugembejohnpaul@gmail.com>
2023-07-19 09:43:06 +0300

    register and login, then trainers models

--- frontend/admin.py
+++ frontend/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Trainer,PharmaceuticalCompany,TrainingProgram,Application
+admin.site.register(Trainer)
+admin.site.register(PharmaceuticalCompany)
+admin.site.register(TrainingProgram)
+admin.site.register(Application)
