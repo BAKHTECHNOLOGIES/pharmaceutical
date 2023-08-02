4204f12e9efc43c03e0d289070f3ac35db60cf85
Ddembe Galiwango Humphrey <galiwangohumphrey55@gmail.com>
2023-08-02 17:34:55 +0300

    First Commit: Created the Contactus Template, Edited the views.py, urls.py files through am still getting an error on the path to contactus.html file

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=158)
+    email = models.EmailField()
+    message = models.TextField()
+
+    def _str_(self):
+        return self.name
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
 def home(request):
     return render(request, "frontend/home.html")
+
+def contactus(request):
+    if request.method == "POST":
+        contact = Contact()
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        contact.name = name
+        contact.email = email
+        contact.message = message
+        contact.save()
+
+    return render(request, "frontend/contactus.html")
