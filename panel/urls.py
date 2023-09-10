658639fa29467cf76040b825244acc2fa79f1ccb
deniskalule <denisjothamzkalule@gmail.com>
2023-09-10 20:41:26 +0300

    worked on adding, editing and deleting categories

--- panel/urls.py
+++ panel/urls.py
@@ -7,7 +7,11 @@ urlpatterns = [
     path('view_client', views.view_client, name="view_client"),
     path('add_trainer', views.add_trainer, name="add_trainer"),
     path('trainers_requests', views.trainers_requests, name="trainers_requests"),
+
     path('add_programcategory', views.add_programcategory, name="add_programcategory"),
+    path('delete_category', views.delete_category, name="delete_category"),
+    path('edit_category', views.edit_category, name="edit_category"),
+
     path('add_training_program', views.add_training_program, name="add_training_program"),
     path('add_client', views.add_client, name="add_client"),
 ]
--- panel/views.py
+++ panel/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
+from django.contrib import messages
 
 # Create your views here.
 def dashboard(request):
@@ -19,14 +21,50 @@ def add_client(request):
         # add_company code 
         return render(request, 'panel/addclient.html')
 
-
+# view for adding a category
 def add_programcategory(request):
-    if request.method == "GET":
-        return render(request, 'panel/program_category.html')
+    if request.method == "POST":
+        category_name = request.POST.get("category_name")
+        this_category = ProgramCategory.objects.filter(category_name=category_name)
+
+        if this_category:
+            messages.error(request,"Category already exists")
+            return redirect("add_programcategory")
+        
+        else:
+            program_category = ProgramCategory.objects.create(category_name=category_name)
+            program_category.save
+            messages.success(request, "Category has successfully been added")
+            return redirect("add_programcategory")
     else:
         # add_programcategory Code
-        return render(request, 'panel/program_category.html')
-    
+        context={
+            "categories": ProgramCategory.objects.all()
+        }
+        return render(request, 'panel/program_category.html', context)
+
+#view for deleting a category
+def delete_category(request):
+    if request.method == "POST":
+        cat_id = request.POST.get("cat_id") 
+        this_category = ProgramCategory.objects.filter(category_id=cat_id)
+
+        this_category.delete()
+        messages.success(request,"Category deleted successfully")
+        return redirect("add_programcategory")
+
+#view for editing category name
+def edit_category(request):
+    if request.method == "POST":
+        cat_id = request.POST.get("cat_id")
+        category_name = request.POST.get("category_name")
+
+        this_category = ProgramCategory.objects.get(category_id=cat_id)
+        this_category.category_name = category_name
+        this_category.save()
+        messages.success(request,"Category edited successfully")
+        return redirect("add_programcategory")
+
 
 def add_training_program(request):
     if request.method == "GET":
