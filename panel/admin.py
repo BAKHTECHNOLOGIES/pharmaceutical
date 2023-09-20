b4eec5327c1dcc1b68c066624f27bac95bad17ff
deniskalule <denisjothamzkalule@gmail.com>
2023-09-20 11:35:56 +0300

    test version

--- panel/admin.py
+++ panel/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import Trainer,Company,ProgramCategory,TrainingProgram,Request
-admin.site.register(Trainer)
-admin.site.register(Company)
-admin.site.register(ProgramCategory)
-admin.site.register(TrainingProgram)
-admin.site.register(Request)
+from .models import *
 
